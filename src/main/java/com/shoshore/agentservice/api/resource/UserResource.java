ec3d14ca3ea91512d1585b5b78b997974429490e
shoshore <tapsshore@gmai.com>
2019-05-03 17:43:57 +0200

    all beans done

--- src/main/java/com/shoshore/agentservice/api/resource/UserResource.java
+++ src/main/java/com/shoshore/agentservice/api/resource/UserResource.java
@@ -1,4 +1,60 @@
 package com.shoshore.agentservice.api.resource;
 
+
+import com.shoshore.agentservice.api.messages.Response;
+import com.shoshore.agentservice.api.processor.api.UserProcessor;
+import com.shoshore.agentservice.repository.api.UserRepository;
+import com.shoshore.agentservice.utils.constants.Constants;
+import com.shoshore.agentservice.utils.messages.external.UserDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+
+
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/users")
+
 public class UserResource {
+
+    private final UserProcessor userProcessor;
+    private final UserRepository userRepository;
+    public UserResource(UserProcessor userProcessor, UserRepository userRepository) {
+        this.userProcessor = userProcessor;
+        this.userRepository = userRepository;
+    }
+    private static Logger LOGGER = LoggerFactory.getLogger(UserResource.class);
+
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Resource successfully created"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+
+@ApiOperation(value = "Create User Record", response = Response.class)
+    @PostMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Response response (@RequestBody final UserDto userDto,
+                              @RequestHeader(value = Constants.SOURCE_NAME_NARRATIVE) final String username,
+                              @ApiParam(value = Constants.LOCALE_LANGUAGE_NARRATIVE) @RequestHeader(value = Constants.LOCALE_LANGUAGE,
+                                      defaultValue = Constants.DEFAULT_LOCALE) final Locale locale){
+
+        LOGGER.info("Received User: {}", userDto.toString());
+
+        return userProcessor.updateUser(userDto, locale, username);
+    }
+
+
+
+
+
 }
--- src/main/java/com/shoshore/agentservice/utils/constants/Constants.java
+++ src/main/java/com/shoshore/agentservice/utils/constants/Constants.java
@@ -2,9 +2,19 @@ package com.shoshore.agentservice.utils.constants;
 
 public class Constants {
 
+    public static final String SOURCE_NAME = "Source-Name";
     public static final int SUCCESS_INT_VALUE = 200;
     public static final int FAILURE_INT_VALUE = 400;
+    public static final String SOURCE_NAME_NARRATIVE = "Source-Name";
+    public static final String LOCALE_LANGUAGE_NARRATIVE = "Internationalisation language, supported ";
+    public static final String LOCALE_LANGUAGE = "Language ";
+    public static final String DEFAULT_LOCALE = "en";
+    public static final String SOURCE_TYPE_NARRATIVE = "Supported";
+    public static final String SOURCE_TYPE = "Source-Type";
     public static final String STATUS_ALL = "All";
+    public static final String RANGE_PATTERN = "dd/MM/yyyy h:mm a";
+    public static final String DATE_RANGE_DELIMITER = " - ";
+    public static final boolean SEARCH_EAGER = true;
 
 
     public Constants() {
